e24e1279cc44991d673c8dac2244af9dadf035a5
oleg_b <oleg.b@cronix.ms>
2018-02-15 19:16:42 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import TextFields from "./components/TextFields";
+
+class App extends Component {
+  render() {
+    return (
+        <TextFields/>
+    );
+  }
+}
+
+export default App;
