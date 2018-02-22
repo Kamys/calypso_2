62d9c828cce04e1e12e4aefac780d9affc9fef94
Nikita <nikita.kamyshenko@mail.ru>
2018-02-22 19:20:21 +0300

    Add modal window

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import registration from './registrationReducer';
 import autorisation from './autorisationReducer';
 import testReducer from "./testReducer";
+import teacherPanel from "./teacherPanelReducer";
 
 export default combineReducers({
 	registration,
     autorisation,
-    testReducer
+    testReducer,
+    teacherPanel
 });
