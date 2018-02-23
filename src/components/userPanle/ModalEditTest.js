911f49e6a0c31a81ab3d9017aa03ab7e3e486870
Kamys <nikita.kamyshenko@mail.ru>
2018-02-23 03:09:47 +0300

    Modal in center

--- src/components/userPanle/ModalEditTest.js
+++ src/components/userPanle/ModalEditTest.js
@@ -15,10 +15,10 @@ const styles = theme => ({
         padding: theme.spacing.unit * 4,
     },
     modal: {
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 0
     }
 });
 
